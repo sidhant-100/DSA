527b4f500e9c77e25e5bf9a957a08ad99063273d
ritwikk10 <141304561+ritwikk10@users.noreply.github.com>
2023-08-03 12:34:57 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "stack.h"
+
+ int main(){
+    stack myStack;
+    init(&myStack);
+
+//push elements onto the stack 
+ push(myStack,10);
+ push(myStack,20);
+ push(myStack,30);
+
+// pop elements from the stack and print them
+ while (!is_empty(&myStack)){
+    int item = pop(&myStack);
+    printf("%d",item);
+
+    }
+ return 0;
+}
+
+
