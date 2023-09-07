3c46da51c37ea41fe465f6b422c143bffff7546d
sidhant-100 <110213731+sidhant-100@users.noreply.github.com>
2023-09-07 15:51:42 +0530

    Add files via upload

--- prog1.c
+++ prog1.c
@@ -1,29 +1,25 @@
 #include<stdio.h>
 #include "postfix.h"
 
-#include "merge.h"
+
 #include "stack.h"
 
 int main()
 { int n,ret;
-char expression[30]= "2 3 1 * + 9 -";
+char expression[30];
 int result;
 Stack s1;
    do
    {
-   	printf("\nenter 1: For merge sort \nenter 2:for insertion sort \n enter 3 for postfix evaluation \nenter 0:for exit");
+   	printf("\nenter 1 for postfix evaluation \nenter 0:for exit");
    	scanf("%d",&n);
    	
    	switch(n)
-   	{ case 1:
-	   mergesort();
-   	         break;
-   	  
-   	  case 2:init(&s1);
-	        push(&s1,50);
-			printf("%d",pop(&s1));
-   	        break;
-		case 3:   
+   	{ 
+   	 
+		case 1:   
+        printf("enter the postfix expression\n");
+        scanf("%s",&expression);
      result= evaluate(expression);  
 printf("Result= %d\n", result);
 break;  
